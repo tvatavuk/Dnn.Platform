471a2113999e47d72f76ff466ddbd6684698c004
Tonći Vatavuk <tonci@sistemi.hr>
2024-10-01 12:56:58 +0200

    Add content workflow tab action buttons in edit mode https://github.com/dnnsoftware/Dnn.Platform/issues/6115

---
+++ Dnn.AdminExperience/EditBar/Dnn.EditBar.UI/Items/DiscardStateMenu.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information
+
+namespace Dnn.EditBar.UI.Items
+{
+    using System;
+
+    using Dnn.EditBar.Library;
+    using Dnn.EditBar.Library.Items;
+    using DotNetNuke.Application;
+    using DotNetNuke.Entities.Content.Common;
+    using DotNetNuke.Entities.Content.Workflow;
+    using DotNetNuke.Entities.Portals;
+    using DotNetNuke.Entities.Tabs;
+    using DotNetNuke.Security.Permissions;
+    using DotNetNuke.Services.Personalization;
+
+    [Serializable]
+    public class DiscardStateMenu : BaseMenuItem
+    {
+        /// <inheritdoc/>
+        public override string Name { get; } = "DiscardState";
+
+        /// <inheritdoc/>
+        public override string Text => "Discard";
+
+        /// <inheritdoc/>
+        public override string CssClass => string.Empty;
+
+        /// <inheritdoc/>
+        public override string Template { get; } = string.Empty;
+
+        /// <inheritdoc/>
+        public override string Parent { get; } = Constants.LeftMenu;
+
+        /// <inheritdoc/>
+        public override string Loader { get; } = "DiscardState";
+
+        /// <inheritdoc/>
+        public override int Order { get; } = 78;
+
+        /// <inheritdoc/>
+        public override bool Visible()
+        {
+            var contentItem = Util.GetContentController().GetContentItem(TabController.CurrentPage.ContentItemId);
+            return Personalization.GetUserMode() == PortalSettings.Mode.Edit
+                   && DotNetNukeContext.Current.Application.SKU == "DNN" // IsPlatform
+                   && TabWorkflowSettings.Instance.IsWorkflowEnabled(PortalSettings.Current.PortalId) // workflow is enabled
+                   && ((WorkflowEngine.Instance.IsWorkflowOnDraft(contentItem) && PermissionProvider.Instance().CanAddContentToPage(TabController.CurrentPage))
+                       || (!WorkflowEngine.Instance.IsWorkflowCompleted(contentItem) && WorkflowSecurity.Instance.HasStateReviewerPermission(contentItem.StateID)));
+        }
+    }
+}
---
+++ Dnn.AdminExperience/EditBar/Dnn.EditBar.UI/Items/DiscardWorkflowMenu.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information
+
+namespace Dnn.EditBar.UI.Items
+{
+    using System;
+
+    using Dnn.EditBar.Library;
+    using Dnn.EditBar.Library.Items;
+    using DotNetNuke.Application;
+    using DotNetNuke.Entities.Content.Common;
+    using DotNetNuke.Entities.Content.Workflow;
+    using DotNetNuke.Entities.Portals;
+    using DotNetNuke.Entities.Tabs;
+    using DotNetNuke.Security.Permissions;
+    using DotNetNuke.Services.Personalization;
+
+    [Serializable]
+    public class DiscardWorkflowMenu : BaseMenuItem
+    {
+        /// <inheritdoc/>
+        public override string Name { get; } = "DiscardWorkflow";
+
+        /// <inheritdoc/>
+        public override string Text => "Reject";
+
+        /// <inheritdoc/>
+        public override string CssClass => string.Empty;
+
+        /// <inheritdoc/>
+        public override string Template { get; } = string.Empty;
+
+        /// <inheritdoc/>
+        public override string Parent { get; } = Constants.LeftMenu;
+
+        /// <inheritdoc/>
+        public override string Loader { get; } = "DiscardWorkflow";
+
+        /// <inheritdoc/>
+        public override int Order { get; } = 80;
+
+        /// <inheritdoc/>
+        public override bool Visible()
+        {
+            var contentItem = Util.GetContentController().GetContentItem(TabController.CurrentPage.ContentItemId);
+            return Personalization.GetUserMode() == PortalSettings.Mode.Edit
+                   && DotNetNukeContext.Current.Application.SKU == "DNN" // IsPlatform
+                   && TabWorkflowSettings.Instance.IsWorkflowEnabled(PortalSettings.Current.PortalId) // workflow is enabled
+                   && !WorkflowEngine.Instance.IsWorkflowCompleted(contentItem) // tab has new version that is not published
+                   && WorkflowSecurity.Instance.HasStateReviewerPermission(contentItem.StateID); // user has workflow approval permission
+        }
+    }
+}
